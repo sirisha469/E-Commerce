8d5cc125e25cc4613d98d0b2e68f0aac958a1fe3
Sirisha Challagiri <100664091+sirisha469@users.noreply.github.com>
2025-03-06 22:17:20 +0530

    Add files via upload
    security started

---
+++ Ecom-User-Service/src/main/java/com/ecom/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ecom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "HI";
+    }
+}
